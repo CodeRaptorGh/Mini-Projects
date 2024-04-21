031ac4c6da7d57a34a2f9d122099f862e21fd679
Code Raptor <35790790+addy1balLack@users.noreply.github.com>
2024-04-21 13:01:38 +0000

    First Commit - Added All Projects to Repo

---
+++ product-modal-2/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors:{
+        bgPrimaryGreen: "#98BFC2",
+        bgDarkBlue: "#022C45"
+      }
+    },
+  },
+  plugins: [],
+};
